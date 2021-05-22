c864573800439c2666ef88454be56e020c908f9c
unknown <kamranalisheeno9@gmail.com>
2021-05-23 00:05:04 +0500

    modified changes

--- src/add-list-component/add.js
+++ src/add-list-component/add.js
@@ -13,12 +13,8 @@ function App() {
   const [count, setCount] = useState([""]);
   const [check, setCheck] = useState(false);
   const [hideBtn, setHideBtn] = useState(false);
-  const [id, setId] = useState();
-  const [addIds, setAddIds] = useState([]);
-  const [id2, setId2] = useState();
   const add =()=>{
     setCount([...count,""])
-    // console.log(count)
     }
     
     const deleteLine = (id)=>{
@@ -28,17 +24,14 @@ function App() {
       console.log(id)
     }
     
-    const checkBox =(e,id)=>{
-      setId(id)
-    }
     
-    const addId = (id)=>{
-      setAddIds([...addIds,id])
-      // console.log(addIds)
-    }
+    // const addId = (id)=>{
+    //   setAddIds([...addIds,id])
+    //   // console.log(addIds)
+    // }
     const hideBtnFunction=(id)=>{
       setHideBtn(!hideBtn)
-      setId2(id)
+      
       // console.log(hideBtn)
     }
 
@@ -46,36 +39,37 @@ function App() {
     <div className="main-container" >
       <h5 className="title"><BsXDiamondFill className="icon" />Media Owner List</h5>
       <div className="border-container">
-
-     {count.map((t,index)=>{
-       return(
-         <div  key={index} className="outer-container"  >
-       <Container fluid  className={ check && id == index ? "input-container changeBorder" : "input-container"}   >
+   
+       <Container fluid  className={ check  ? "input-container changeBorder" : "input-container"}   >
  
   <Row  className="justify-content-start row-1">
     <Col xl={10} lg={10} md={10} sm={10} xs={10} className="col-checkBox">
     <Form.Group  controlId="formBasicCheckbox">
-    <Form.Check onClick={(e)=>checkBox(e,index)} className="check-box" onChange={(e)=>setCheck(e.target.checked)} type="checkbox" label="Clear Channel" />
+    <Form.Check className="check-box" onChange={(e)=>setCheck(e.target.checked)} type="checkbox" label="Clear Channel" />
   </Form.Group>
     </Col>
     <Col xl={2} lg={2} md={2} sm={2} xs={2} >
       
-    <Button onClick={()=>hideBtnFunction(index)} className={hideBtn && id2 == index ? "down" : "toggle-btn"}>
+    <Button onClick={()=>hideBtnFunction()}  className={hideBtn  ? "down" : "toggle-btn"}>
 <FaChevronDown  />
 </Button>
 
 
     </Col>
   </Row>
-<Row className={hideBtn && id2 == index ? "row-2" :  "hide"}>
-  <Col xl={4} lg={4} md={4} sm={6} xs={6} >
+<div className={hideBtn  ? "row-2" :  "hide"}>
+  {count.map((t,index)=>{
+    return(
+        <div key={index}>
+            <Row>
+
+  <Col  xl={4} lg={4} md={4} sm={6} xs={6} >
   <Form.Group>
   <Form.Control as="select">
-    <option>Formaat</option>
+    <option>LED Ekraan</option>
   </Form.Control>
   
 </Form.Group>
-  <NavLink className="add-line" onClick={(e)=>add(e)}>Lisa Rohkem</NavLink>
   </Col>
   <Col xl={4} lg={4} md={4} sm={6} xs={6}>
   <Form.Group>
@@ -86,7 +80,7 @@ function App() {
 
 </Form.Group>
   </Col>
-  <Col xl={3} lg={3} md={3} sm={10} xs={10}>
+  <Col className="changes" xl={3} lg={3} md={3} sm={10} xs={10}>
   <Form.Group>
   <InputGroup className="mb-2 mr-sm-2">
     <FormControl id="inlineFormInputGroupUsername2" placeholder="Allahindulus" />
@@ -96,21 +90,28 @@ function App() {
   </InputGroup>
 </Form.Group>
   </Col>
-  <Col xl={1} lg={1} md={1} sm={2} xs={2} >
-  <InputGroup onClick={(e)=>deleteLine(index)} className="mb-2 mr-sm-2 delete">
+  <Col className="changes" xl={1} lg={1} md={1} sm={2} xs={2} >
+  <InputGroup onClick={(e)=>deleteLine(index)}  className="mb-2 mr-sm-2 delete">
     <InputGroup.Prepend>
       <InputGroup.Text> <AiOutlineDelete className="delete-icon" /></InputGroup.Text>
     </InputGroup.Prepend>
   </InputGroup>
 </Col>
 </Row>
+
+        </div>
+   
+   ) 
+   
+})}
+  <NavLink className="add-line" onClick={(e)=>add(e)}>Lisa Rohkem</NavLink>
+
+</div>
+
 </Container>
          </div>
        
-       )
-      })}
-      </div>
-      
+     
     </div>
   );
 }
